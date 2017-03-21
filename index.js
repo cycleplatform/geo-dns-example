41da7937234875cd7f94dfff18bdd21005f52da9
amattoni <amattoni@petrichor.io>
2017-03-21 11:31:46 -0700

    var -> const

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ spdy
     });
 
 // Redirect to https
-var http = require('http');
+const http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
     res.end();
